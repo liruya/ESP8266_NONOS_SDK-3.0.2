3e3b1a3805760c40e3f4b0c220cd840c9efa85d5
liruya <cylary1218@gmail.com>
2020-03-13 18:07:07 +0800

    20200313

--- app/user/user_rtc.c
+++ app/user/user_rtc.c
@@ -3,20 +3,64 @@
 
 #define	SYNC_TIME_PERIOD			60			//同步时间周期 60秒
 
+#define	EPOCH_YEAR					1970
+#define	EPOCH_WEEKDAY				4			//1970.1.1 weekday
+#define	SECONDS_PER_MINUTE			60
+#define	MINUTES_PER_HOUR			60
+#define	HOURS_PER_DAY				24
+#define	SECONDS_PER_HOUR			(SECONDS_PER_MINUTE*MINUTES_PER_HOUR)
+#define	SECONDS_PER_DAY				(SECONDS_PER_MINUTE*MINUTES_PER_HOUR*HOURS_PER_DAY)
+#define	MINUTES_PER_DAY				(MINUTES_PER_HOUR*HOURS_PER_DAY)
+#define	DAYS_PER_WEEK				7
+
 static const char *TAG = "RTC";
+
+const uint8_t days_normal_year[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+const uint8_t days_leap_year[12] = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
 static uint64_t current_time;
 static bool synchronized;
 static os_timer_t timer;
 static int sync_period_cnt;
+char current_time_str[24];
+static int device_zone;
+
+ICACHE_FLASH_ATTR static bool is_leap_year(uint16_t year) {
+	if (year%4 != 0) {
+		return false;
+	}
+	if (year%100 == 0 && year%400 != 0) {
+		return false;
+	}
+	return true;
+}
+
+ICACHE_FLASH_ATTR static bool valid_zone(int zone) {
+	if (zone < -720 || zone > 720) {
+		return false;
+	}
+	return true;
+}
+
+ICACHE_FLASH_ATTR void user_rtc_set_zone(int zone) {
+	if (zone < -720 || zone > 720) {
+		return;
+	}
+	device_zone = zone;
+}
 
 ICACHE_FLASH_ATTR static void user_rtc_process(void *arg) {
+	date_time_t datetime;
 	current_time += 1000;
+	os_memset(current_time_str, 0, sizeof(current_time_str));
+	uint32_t v1 = current_time/100000000;
+	uint32_t v2 = current_time%100000000;
+	os_sprintf(current_time_str, "%d%d", v1, v2);
 	sync_period_cnt++;
 	if (sync_period_cnt >= SYNC_TIME_PERIOD) {
 		sync_period_cnt = 0;
 		aliot_mqtt_get_sntptime();
-		LOGD(TAG, "current time: %lld", current_time);
+		LOGD(TAG, "current time: %s  %lld", current_time_str, current_time);
 	}
 }
 
@@ -45,5 +89,60 @@ ICACHE_FLASH_ATTR void user_rtc_sync_time() {
 		os_timer_setfn(&timer, user_rtc_sync_fn, NULL);
 		os_timer_arm(&timer, 5000, 1);
 	}
+}
+
+ICACHE_FLASH_ATTR bool user_rtc_get_datetime(date_time_t *datetime, int zone) {
+	if (zone < -720 || zone > 720) {
+		return false;
+	}
+	uint64_t time = current_time/1000 + zone*60;
+	uint32_t seconds = time%SECONDS_PER_DAY;
+	uint32_t days = time/SECONDS_PER_DAY;
+	datetime->hour = seconds/SECONDS_PER_HOUR;
+	seconds %= SECONDS_PER_HOUR;
+	datetime->second = seconds%SECONDS_PER_MINUTE;
+	datetime->minute = seconds/SECONDS_PER_MINUTE;
+	datetime->weekday = (days+EPOCH_WEEKDAY)%DAYS_PER_WEEK;
+	uint16_t year = EPOCH_YEAR;
+	uint16_t month;
+	uint16_t days_of_year;
+	bool yleap;
+	int i;
+	while (1) {
+		yleap = is_leap_year(year);
+		if (yleap) {
+			days_of_year = 366;
+		} else {
+			days_of_year = 365;
+		}
+		if (days < days_of_year) {
+			datetime->year = year;
+			if (yleap) {
+				for (i = 0; i < sizeof(days_leap_year); i++) {
+					if (days < days_leap_year[i]) {
+						datetime->month = i+1;
+						datetime->day = days;
+						return true;
+					} else {
+						days -= days_leap_year[i];
+					}
+				}
+			} else {
+				for (i = 0; i < sizeof(days_leap_year); i++) {
+					if (days < days_normal_year[i]) {
+						datetime->month = i+1;
+						datetime->day = days;
+						return true;
+					} else {
+						days -= days_normal_year[i];
+					}
+				}
+			}
+			break;
+		}
+		days -= days_of_year;
+		year++;
+	}
+	return false;
 }
 
--- app/user/user_rtc.h
+++ app/user/user_rtc.h
@@ -4,8 +4,19 @@
 #include "aliot_mqtt.h"
 #include "app_common.h"
 
+typedef struct {
+	uint16_t	year;
+	uint8_t 	month;
+	uint8_t 	day;
+	uint8_t 	weekday;
+	uint8_t 	hour;
+	uint8_t 	minute;
+	uint8_t 	second;
+} date_time_t;
+
 extern	void user_rtc_set_time(const uint64_t time);
 extern	uint64_t user_rtc_get_time();
 extern	void user_rtc_sync_time();
+extern	bool user_rtc_get_datetime(date_time_t *datetime, int zone);
 
 #endif
